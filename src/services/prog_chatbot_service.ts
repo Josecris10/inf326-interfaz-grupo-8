0143548d5f651fba46f61e1ccc7e5df3fbfac5c5
JanJorquera <janjorquera@gmail.com>
2025-11-19 21:29:36 -0300

    [Add] comunicación con chatbot de programación

---
+++ src/services/prog_chatbot_service.ts
@@ -0,0 +1,44 @@
+import { gqlQuery } from "./common";
+
+const API_URL = import.meta.env.VITE_API_GATEWAY_URL;
+const GRAPHQL_PATH = "";
+
+// ===================================================================
+//  Definiciones de tipos de las respuestas GraphQL (data)
+// ===================================================================
+
+export interface PrograChatbotReply {
+    reply: string;
+}
+
+type GetMessagePrograChatbot = {
+    getMessagePrograChatbot: PrograChatbotReply;
+}
+
+// ===================================================================
+//  Queries GraphQL (STRINGS)
+// ===================================================================
+
+const GET_MESSAGE_QUERY = /* GraphQL */ `
+    query GetMessagePrograChatbot($message: String!) {
+        getMessagePrograChatbot(message: $message) {
+            reply
+        }
+    }
+`;
+
+//============================== Funciones de Servicio ==============================
+
+export async function getMessagePrograChatbot(
+    message: string
+): Promise<PrograChatbotReply> {
+
+    const data = await gqlQuery<GetMessagePrograChatbot>(
+        API_URL,
+        GRAPHQL_PATH,
+        GET_MESSAGE_QUERY,
+        { message }
+    );
+
+    return data.getMessagePrograChatbot; // ← CORRECTO
+}
